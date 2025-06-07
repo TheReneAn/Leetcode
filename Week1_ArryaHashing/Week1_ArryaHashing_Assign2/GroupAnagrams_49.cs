11ed6cb841912878e77246e0de6486a8b4fe6963
Rene <rene@shiftyeyegames.com>
2025-06-06 20:05:44 -0600

    Week1

---
+++ Week1_ArryaHashing/Week1_ArryaHashing_Assign2/GroupAnagrams_49.cs
@@ -0,0 +1,25 @@
+﻿namespace Week1_ArrayHashing
+{
+    class GroupAnagrams_49
+    {
+        static void Main(string[] args)
+        {
+            string[] strs1 = ["eat", "tea", "tan", "ate", "nat", "bat"];
+            string[] strs2 = [" "];
+            string[] strs3 = ["a"];
+
+            // Answer_1
+            Console.WriteLine("Answer 1:");
+            // var resultAnswer1 = Answer1_GroupAnagrams(strs1);
+
+            // Answer_2
+            Console.WriteLine("Answer 2:");
+
+        }
+
+        // static IList<IList<string>> Answer1_GroupAnagrams(string[] strs) 
+        // {
+        //     
+        // }
+    }
+}
---
+++ Week1_ArryaHashing/Week1_ArryaHashing_Assign3/TwoSum_1.cs
@@ -0,0 +1,10 @@
+﻿namespace Week1_ArrayHashing
+{
+    class TwoSum_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
