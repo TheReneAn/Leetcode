d1116d6c1b58548d20d4e0ff4c53567325fd8fa0
Rene <auj0827@gmail.com>
2025-07-14 19:30:37 -0600

    Week7_Backtracking

---
+++ Week7_Backtracking/Week7_Backtracking_Assign2/Permutations_46.cs
@@ -0,0 +1,54 @@
+﻿/***************************************************************
+ * 🔷 LeetCode 46. Permutations
+ *
+ * 🟡 Difficulty: Medium
+ *
+ * 📘 Problem:
+ *   Given an array `nums` of distinct integers, return all possible
+ *   permutations. You can return the answer in any order.
+ *
+ * 📥 Example 1:
+ *   Input:  nums = [1,2,3]
+ *   Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ * 📥 Example 2:
+ *   Input:  nums = [0,1]
+ *   Output: [[0,1],[1,0]]
+ *
+ * 📥 Example 3:
+ *   Input:  nums = [1]
+ *   Output: [[1]]
+ *
+ * ✅ Constraints:
+ *   - 1 <= nums.length <= 6
+ *   - -10 <= nums[i] <= 10
+ *   - All the integers of `nums` are unique.
+ *
+ * 🚩 Topics:
+ *   Array, Backtracking
+ ***************************************************************/
+
+using System.Diagnostics;
+
+namespace Week7_Backtracking_Assign2;
+
+public class Subsets_78
+{
+    private static void Main()
+    {
+        
+    }
+    
+    public IList<IList<int>> Permute(int[] nums) 
+    {
+        
+    }
+    
+    private static void MeasureExecutionTime(Action action)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        action();
+        stopwatch.Stop();
+        Console.WriteLine($"Execution Time: {stopwatch.Elapsed.TotalMilliseconds} ms\n");
+    }
+}
---
+++ Week7_Backtracking/Week7_Backtracking_Assign3/CombinationSum_39.cs
@@ -0,0 +1,63 @@
+﻿/***************************************************************
+ * 🔷 LeetCode 39. Combination Sum
+ *
+ * 🟡 Difficulty: Medium
+ *
+ * 📘 Problem:
+ *   Given an array of distinct integers candidates and a target integer target,
+ *   return a list of all unique combinations of candidates where the chosen numbers
+ *   sum to target. You may return the combinations in any order.
+ *
+ *   The same number may be chosen from candidates an unlimited number of times.
+ *   Two combinations are unique if the frequency of at least one of the chosen
+ *   numbers is different.
+ *
+ *   The test cases are generated such that the number of unique combinations that
+ *   sum up to target is less than 150 combinations for the given input.
+ *
+ * 📥 Example 1:
+ *   Input:  candidates = [2,3,6,7], target = 7
+ *   Output: [[2,2,3], [7]]
+ *
+ * 📥 Example 2:
+ *   Input:  candidates = [2,3,5], target = 8
+ *   Output: [[2,2,2,2], [2,3,3], [3,5]]
+ *
+ * 📥 Example 3:
+ *   Input:  candidates = [2], target = 1
+ *   Output: []
+ *
+ * ✅ Constraints:
+ *   - 1 <= candidates.length <= 30
+ *   - 2 <= candidates[i] <= 40
+ *   - All elements of candidates are distinct
+ *   - 1 <= target <= 40
+ *
+ * 🚩 Topics:
+ *   Array, Backtracking
+ ***************************************************************/
+
+using System.Diagnostics;
+
+namespace Week7_Backtracking_Assign3;
+
+public class CombinationSum_39
+{
+    private static void Main()
+    {
+        
+    }
+    
+    public IList<IList<int>> CombinationSum(int[] candidates, int target) 
+    {
+        
+    }
+    
+    private static void MeasureExecutionTime(Action action)
+    {
+        var stopwatch = Stopwatch.StartNew();
+        action();
+        stopwatch.Stop();
+        Console.WriteLine($"Execution Time: {stopwatch.Elapsed.TotalMilliseconds} ms\n");
+    }
+}
